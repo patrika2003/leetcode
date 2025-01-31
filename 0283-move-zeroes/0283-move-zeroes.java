311c8638e15e06456037ecafd304765cc1b0e0ba
Patrika Chatterjee <144355221+patrika2003@users.noreply.github.com>
2025-01-31 16:20:37 +0530

    Time: 2 ms (84.87%), Space: 46.4 MB (5.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int insert_pos=0;
+        int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[insert_pos]=nums[i];
-                insert_pos++;
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                j++;
             }
-        }
-        while(insert_pos<nums.length){
-            nums[insert_pos++]=0;
         }
     }
 
